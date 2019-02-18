8bfdbe0942a078ab6046ef45b1e70cee7c880e8c
Андрей <sizv1996@hotmail.com>
2019-02-18 22:21:49 +0300

    passive & dihomy

---
+++ Optimization_Lab1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Optimization_Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(67, 26);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Legend1";
+            series1.Name = "График";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(584, 279);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(373, 353);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Расчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(657, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Интервал";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(660, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "0 1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(660, 87);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "0,0011";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(657, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Epsilon";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(660, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Шаги";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(660, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 412);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Lab 1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Optimization_Lab1/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Optimization_Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private double epsilon;
+        private double max;
+        private double min;
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Dictionary<double, double> passive = new Dictionary<double, double>();
+            min = Convert.ToDouble(textBox1.Text.Split(' ')[0]);
+            max = Convert.ToDouble(textBox1.Text.Split(' ')[1]);
+            epsilon = Convert.ToDouble(textBox2.Text);
+            FillPassive(passive, epsilon);
+            Console.Write(passive);
+            Draw(passive);
+            PassiveSearch(passive);
+            dihomySearch(epsilon, 0.0005);
+        }
+
+        private void PassiveSearch(Dictionary<double, double> passive)
+        {
+            double localMinY;
+            double firstX = min;
+            double firstY = Function(firstX);
+            localMinY = passive[min];
+            double localMinX = min;
+
+            foreach (var el in passive)
+                if (el.Value < localMinY)
+                {
+                    localMinX = el.Key;
+                    localMinY = el.Value;
+                }
+
+            PutSolutionPassive(localMinX, localMinY);
+        }
+
+        private void dihomySearch(double epsilon, double delta)
+        {
+            double error = Math.Abs(max - min);
+            double alpha = 0;
+            double a = min;
+            double af;
+            double betta;
+            double b = max;
+            double bf;
+
+            while (error >= epsilon)
+            {
+                alpha = (a + b) / 2 - delta;
+                betta = (a + b) / 2 + delta;
+                af = Function(alpha);
+                bf = Function(betta);
+                if (af <= bf)
+                {
+                    b = betta;
+                }
+                else
+                {
+                    a = alpha;
+                }
+                error = b - a;
+            }
+            PutSolutionDihomy(alpha, Function(alpha));
+        }
+
+        private void Draw(Dictionary<double, double> valuePairs)
+        {
+            foreach (var element in valuePairs)
+            {
+                chart1.Series[0].Points.AddXY(element.Key, element.Value);
+            }
+        }
+
+        private void PutSolutionPassive(double x, double y)
+        {
+            string name = "Решение пассивным поиском - " + Math.Round(x, 4);
+            try
+            {
+                chart1.Series.Add(name);
+            }
+            catch (ArgumentException) { }
+            finally
+            {
+                chart1.Series[name].Color = Color.Red;
+                chart1.Series[name].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+                chart1.Series[name].Points.AddXY(x, y);
+            }
+        }
+
+        private void PutSolutionDihomy(double x, double y)
+        {
+            string name = "Решение дихомией - " + Math.Round(x, 4);
+            try
+            {
+                chart1.Series.Add(name);
+            }
+            catch (ArgumentException) { }
+            finally
+            {
+                chart1.Series[name].Color = Color.Green;
+                chart1.Series[name].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+                chart1.Series[name].Points.AddXY(x, y);
+            }
+        }
+
+        private double Function(double x)
+        {
+            return Math.Pow(x, 3) - x + Math.Exp(-x);
+        }
+
+        private void FillPassive(Dictionary<double, double> dict, double epsilon)
+        {
+            int numbers = 1;
+            double distance = Math.Abs(max - min);
+            double copy = epsilon;
+            while (copy < 1)
+            {
+                numbers *= 10;
+                copy *= 10;
+            }
+            label4.Text = numbers.ToString();
+            double step = distance / numbers;
+
+            for (int i = 0; i <= numbers; i++)
+            {
+                double x = min + i * step;
+                double y = Function(x);
+                dict.Add(x, y);
+            }
+        }
+    }
+}
